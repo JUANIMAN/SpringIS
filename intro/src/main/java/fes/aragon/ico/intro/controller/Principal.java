da7c163d2411ff64b64d9fd980c825b45d73d9f9
JUANIMAN <juanimandarin9@gmail.com>
2023-05-03 02:04:34 -0600

    Se cambiaron actualizo el formato del salario

--- intro/src/main/java/fes/aragon/ico/intro/controller/Principal.java
+++ intro/src/main/java/fes/aragon/ico/intro/controller/Principal.java
@@ -10,10 +10,10 @@ import java.util.Date;
 public class Principal {
     @GetMapping("/inicio")
     public String getInicio(Model model) {
-        Empleado emp = new Empleado("Juan Perez", 1, 2500.00, "Programador", "Desarrollo de Software", "09/10/2002");
-        Empleado emp1 = new Empleado("John Doe", 2, 50000, "Gerente de ventas", "Ventas", "20/08/2000");
-        Empleado emp2 = new Empleado("Jane Smith", 3, 40000, "Analista de datos", "Tecnología", "05/2/1998");
-        Empleado emp3 = new Empleado("Mike Johnson", 4, 60000, "Gerente de operaciones", "Operaciones", "07/12/1995");
+        Empleado emp = new Empleado("Juan Perez", 1, "$25000", "Programador", "Desarrollo de Software", "09/10/2002");
+        Empleado emp1 = new Empleado("John Doe", 2, "$50000", "Gerente de ventas", "Ventas", "20/08/2000");
+        Empleado emp2 = new Empleado("Jane Smith", 3, "$40000", "Analista de datos", "Tecnología", "05/2/1998");
+        Empleado emp3 = new Empleado("Mike Johnson", 4, "$60000", "Gerente de operaciones", "Operaciones", "07/12/1995");
 
 
         model.addAttribute("empleado", emp);
